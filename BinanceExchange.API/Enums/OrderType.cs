02cf1c71c6eb41bafd628c836d4c4ae2a2868b1e
Konstantin Spirin <konstmsu@gmail.com>
2018-01-26 02:23:58 +0800

    Fixed OrderType parsing issue

--- BinanceExchange.API/Enums/OrderType.cs
+++ BinanceExchange.API/Enums/OrderType.cs
@@ -1,12 +1,18 @@
-﻿using System.Runtime.Serialization;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
 
 namespace BinanceExchange.API.Enums
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum OrderType
     {
+        None,
         [EnumMember(Value = "LIMIT")]
         Limit,
         [EnumMember(Value = "MARKET")]
         Market,
+        [EnumMember(Value = "TAKE_PROFIT_LIMIT")]
+        TakeProfitLimit,
     }
 }
